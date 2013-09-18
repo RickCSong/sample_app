6d679241ca7b739d46f6eb35b9b0c9de86d8cade
Rick Song <ricksong@squareup.com>
2013-09-18 00:22:59 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static Pages" do
 
   subject { page }
 
@@ -16,20 +16,20 @@ describe "StaticPages" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    # it { should have_title(full_title('Help')) }
+    it { should have_title(full_title('Help')) }
   end
 
   context "About page" do
     before { visit about_path }
 
     it { should have_content('About Us') }
-    # it { should have_title(full_title('About Us')) }
+    it { should have_title(full_title('About Us')) }
   end
 
   context "Contact page" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    # it { should have_title(full_title('About Us')) }
+    it { should have_title(full_title('Contact')) }
   end
 end
