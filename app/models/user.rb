4c7cfcab8471df0a9429b3cd261b138cd357b3fc
Rick Song <ricksong@squareup.com>
2013-09-22 00:28:31 -0700

    Finished user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  before_save { email.downcase! }
 
   validates :name,
     presence: true,
